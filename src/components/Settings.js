92a7606d6b31795c15be364a676013a58383db5a
Xavi <>
2018-08-22 20:21:50 +0200

    dd

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -8,22 +8,18 @@ class Settings extends Component {
   constructor(props){
     super(props);
   }
-
-  addFormFields(){
-    this.props.products.map(
-
-    )
+  save() {
+    console.log(this.props);
   }
-
   render() {
     return(
       <div className="Header">
         <h2>Configuration</h2>
         <Form>
         {this.props.products.map((product, index)=>{
-          return <Form.Field><Input key={index} action={{ color: 'teal', labelPosition: 'right', icon: 'plus'}} placeholder={product} /></Form.Field>
+          return <Form.Input key={index} action={{ color: 'teal', labelPosition: 'right', icon: 'plus'}} placeholder={product}/>
         })}
-        <Form.Button onClick={() => this.props.onLogOut()}>Save</Form.Button>
+        <Form.Button onClick={() => this.save()}>Save</Form.Button>
           <Form.Button onClick={() => this.props.onLogOut()}>Log out</Form.Button>
         </Form>
       </div>
