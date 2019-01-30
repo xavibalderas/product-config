7da31b1a5df1cfc0b1d93671c4d0326a6579b176
Xavi <xavibalderas@gmail.com>
2019-01-30 14:24:18 +0100

    d

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const URI_API = process.env.REACT_APP_GRAPHCMS_API;
 
 const client = new ApolloClient({
       //uri: 'https://graphqlserver-productsinfo.herokuapp.com/'
-
       uri: URI_API
 
 })
